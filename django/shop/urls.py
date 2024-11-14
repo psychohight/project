e1523bf7c7f0de315d561161315f163a7ad0699e
psychohight <7298@holbertonstudents.com>
2024-11-14 18:57:39 +0100

    boutique visible

--- django/shop/urls.py
+++ django/shop/urls.py
@@ -24,6 +24,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
--- django/store/urls.py
+++ django/store/urls.py
@@ -5,9 +5,8 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.index, name='index'),  # Route pour la page d'accueil
-    path('boutique/', views.index, name='boutique'),  # Route pour la page de boutique (par exemple)
+    path('boutique/', views.boutique, name='boutique'),  # Route pour la page de boutique (par exemple)
     path('enregistrement/', views.enregistrement, name='enregistrement'), # Route pour la page d'enregistrement
     path('panier/', views.panier, name='panier'), # Route pour la page du panier
-    
-    
+       
 ]
